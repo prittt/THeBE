94c676bc9253c23c7b5b2819ef232e902308c712
CostantinoGrana <costantino.grana@unimore.it>
2019-04-09 03:26:19 +0200

    Fix GuoHall (was still ZhangSuen).

--- src/thinning_guohall_1989.cc
+++ src/thinning_guohall_1989.cc
@@ -41,33 +41,32 @@ REGISTER_THINNING(GuoHallLUT);
     const uchar p8 = (block >> 3) & 1;  \
     const uchar p9 = (block >> 0) & 1;  \
 
-inline bool GuoHall::should_remove_0(uint16_t block) {
+inline bool GuoHall::should_remove_0(uint16_t block) 
+{
     BLOCK_TO_P
 
-        int A = (p2 == 0 && p3 == 1) + (p3 == 0 && p4 == 1) +
-        (p4 == 0 && p5 == 1) + (p5 == 0 && p6 == 1) +
-        (p6 == 0 && p7 == 1) + (p7 == 0 && p8 == 1) +
-        (p8 == 0 && p9 == 1) + (p9 == 0 && p2 == 1);
-    int B = p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9;
-    int m1 = p2 * p4 * p6;
-    int m2 = p4 * p6 * p8;
+    int C = ((!p2) & (p3 | p4)) + ((!p4) & (p5 | p6)) +
+        ((!p6) & (p7 | p8)) + ((!p8) & (p9 | p2));
+    int N1 = (p9 | p2) + (p3 | p4) + (p5 | p6) + (p7 | p8);
+    int N2 = (p2 | p3) + (p4 | p5) + (p6 | p7) + (p8 | p9);
+    int N = N1 < N2 ? N1 : N2;
+    int m = (p6 | p7 | (!p9)) & p8;
 
-    return A == 1 && (B >= 2 && B <= 6) && m1 == 0 && m2 == 0;
+    return (C == 1) && ((N >= 2) && ((N <= 3)) & (m == 0));
 }
 
 inline bool GuoHall::should_remove_1(uint16_t block)
 {
     BLOCK_TO_P
 
-        int A = (p2 == 0 && p3 == 1) + (p3 == 0 && p4 == 1) +
-        (p4 == 0 && p5 == 1) + (p5 == 0 && p6 == 1) +
-        (p6 == 0 && p7 == 1) + (p7 == 0 && p8 == 1) +
-        (p8 == 0 && p9 == 1) + (p9 == 0 && p2 == 1);
-    int B = p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9;
-    int m1 = p2 * p4 * p8;
-    int m2 = p2 * p6 * p8;
+    int C = ((!p2) & (p3 | p4)) + ((!p4) & (p5 | p6)) +
+        ((!p6) & (p7 | p8)) + ((!p8) & (p9 | p2));
+    int N1 = (p9 | p2) + (p3 | p4) + (p5 | p6) + (p7 | p8);
+    int N2 = (p2 | p3) + (p4 | p5) + (p6 | p7) + (p8 | p9);
+    int N = N1 < N2 ? N1 : N2;
+    int m = (p2 | p3 | (!p5)) & p4;
 
-    return A == 1 && (B >= 2 && B <= 6) && m1 == 0 && m2 == 0;
+    return (C == 1) && ((N >= 2) && ((N <= 3)) & (m == 0));
 }
 
 void GuoHall::PerformThinning()
